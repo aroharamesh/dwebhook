6cf5e380f7ec020798f6f98f18306a6480f38785
Ramesh <ramesh@aroha.co.in>
2022-04-11 13:24:25 +0530

    added nginx

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 server {
     listen 80;
-    server_name api.productivity.aroha.co.in;
-    access_log /var/log/nginx/api_productivity.access.log;
-    error_log /var/log/nginx/api_productivity.error.log;
+    server_name capability.aroha.co.in;
+    rewrite ^ https://capability.aroha.co.in/$1 permanent;
+}
+server {
+    listen 443 ssl;
+    #listen 80;
+    server_name capability.aroha.co.in;
+
+    #ssl on;
+    ssl_certificate /etc/letsencrypt/live/capability.aroha.co.in-0001/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/capability.aroha.co.in-0001/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+    access_log /var/log/nginx/capability_ssl.access.log;
+    error_log /var/log/nginx/capability_ssl.error.log;
     location / {
-        proxy_pass http://ec2-54-148-166-223.us-west-2.compute.amazonaws.com:8047;
+        proxy_pass http://ec2-54-148-166-223.us-west-2.compute.amazonaws.com:5000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection 'upgrade';
@@ -12,6 +25,4 @@ server {
         proxy_cache_bypass $http_upgrade;
         client_max_body_size 100M;
     }
-
-
 }
