2dcc39af423d5e749f92a01c92122fb53a3c341e
Ramesh <ramesh@aroha.co.in>
2022-04-12 11:43:00 +0530

    updated code

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ app = FastAPI()
 
 def response_to_dict(response):
     """Converting bytes response to python dictionary"""
-    response_content = response.content
-    response_decode = response_content.decode("UTF-8")
+    # response_content = response.content
+    response_decode = response.decode("UTF-8")
     json_acceptable_string = response_decode.replace("'", "\"")
     convert_to_json = json.loads(json_acceptable_string)
     response_dict = dict(convert_to_json)
