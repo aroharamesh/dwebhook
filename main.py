457d68d7b07afed86dce80ad1160369935ea9ec3
Ramesh <ramesh@aroha.co.in>
2022-04-12 11:40:44 +0530

    updated code

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request, Body
 import json
 from Crypto.Cipher import AES
 from base64 import b64decode
+import uvicorn
+from typing import Dict
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+def response_to_dict(response):
+    """Converting bytes response to python dictionary"""
+    response_content = response.content
+    response_decode = response_content.decode("UTF-8")
+    json_acceptable_string = response_decode.replace("'", "\"")
+    convert_to_json = json.loads(json_acceptable_string)
+    response_dict = dict(convert_to_json)
+    return response_dict
+
+
+@app.post("/")
+async def root(payload: Dict = Body(...)):
+    print('hello coming here', payload)
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
+@app.post("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
 @app.post("/lotuspay")
-async def lotus_pay_webhook():
-    msg = json.loads('DvaraWebHook')['serialized_response']
+async def lotus_pay_webhook(payload: Dict = Body(...), status_code=status.HTTP_200_OK):
+    #msg = json.loads('sk_test_5kCfPu3Wx6VBNZsbc6a6TibS')
+    #payload_response = {"serialized_response":"5kCfPu3Wx6VBNZsbc6a6TibS"}
+    #msg = json.loads(payload_response)
+    #msg =
+    msg = payload['serialized_response']
+    print('printing serialized response - ', msg)
+    #msg = json.loads(payload)['serialized_response']
+    #iv = msg[:16].encode('utf-8')
     iv = msg[:16].encode('utf-8')
-    decipher = AES.new('whk_:PqnYy_VUz7Z', AES.MODE_CBC, iv=iv)
+    decipher = AES.new('whk_:PqnYy_VUz7Z'.encode('utf-8'), AES.MODE_CBC, iv=iv)
     cipher_text = b64decode(msg[16:].encode('utf-8'))
+    #cipher_text = b64decode(msg[16:].encode('utf-8'))
     data = decipher.decrypt(cipher_text)
-    event = json.loads(data[:data.rfind('}')+1])
-    return event
+    #event = json.loads(data[:data.rfind('}')+1])
+    print(data)
+    data_dict = response_to_dict(data)
+    return data_dict
+    # return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
